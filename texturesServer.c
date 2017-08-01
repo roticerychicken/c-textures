1fd2286725fc7e2f5a980978226e8c59d7f64f74
sk8ertag <sk8ertag@localhost.localdomain>
2017-08-01 18:59:02 -0400

    finally got it to be multi cliented

--- texturesServer.c
+++ texturesServer.c
@@ -17,38 +17,40 @@
 //size of the buffer
 #define MAX_DATA	1024
 
-struct sockaddr_in server;
-struct sockaddr_in client;
-int sock;
 
-int sockaddr_len = sizeof(struct sockaddr_in);
-int data_len;
-char data[MAX_DATA];
+
 
 void* run_server(void* arg)
 {
 	long new = (long)arg;
+	int data_len;
+	char data[MAX_DATA];
+	
 	while(1) {
+		
 		data_len = 1;	
 		while(data_len){
 			data_len = recv(new,data,MAX_DATA,0);
-			if(data_len) {
+			printf("sent msg: %s\n",data);
+			if(data_len >= 2) {
 				data[data_len] = '\0';
-				//printf("sent msg: %s\n",data);
+				printf("sent msg: %s\n",data);
 			}	
 		}
 		close(new);
 	}
-	close(sock);
+
+
+
 }
 int main(int argc,char *argv[]) {
-	int new;
 
-	if((sock = socket(AF_INET, SOCK_STREAM, 0)) == ERROR)
-	{
-		perror("socket: ");
-		exit(1);
-	}
+	struct sockaddr_in server;
+	struct sockaddr_in client;
+	int server_sock = socket(AF_INET,SOCK_STREAM,0);
+	long client_sock;
+	int sockaddr_len = sizeof(struct sockaddr_in);
+	
 
 
 	server.sin_family = AF_INET;
@@ -56,34 +58,31 @@ int main(int argc,char *argv[]) {
     	//inaddr instructs the kernal to listen on all interfaces
     	server.sin_addr.s_addr = INADDR_ANY;
     	bzero(&server.sin_zero,8);
+
+	pthread_t tid[5];
+	pthread_attr_t attr;
+	pthread_attr_init(&attr);
+
 	
-	if ((bind(sock, (struct sockaddr *)&server, sockaddr_len)) == ERROR) {
-        	perror("bind :");
-        	exit(-1);
-    	}
-
-	if ((listen(sock, MAX_CLIENTS)) == ERROR) {
-        	perror("listen");
-        	exit(-1);
-   	}
-	while(1) {
-		
-		if (!(new = accept(sock, (struct sockaddr *)&client, &sockaddr_len)) == ERROR) {
-			perror("accept");
-			pthread_t tid;
-			pthread_attr_t attr;
-			pthread_attr_init(&attr);
-
-			pthread_create(&tid,&attr,run_server,&new);
-			pthread_join(tid,NULL);
+	if ((bind(server_sock, (struct sockaddr *)&server, sockaddr_len)) >= ERROR) {
+        	printf("socket created");
+		if ((listen(server_sock, MAX_CLIENTS)) == ERROR) {
+			perror("listen");
 			exit(-1);
+   		}
+		while(1)
+		{
+			for(int i = 0; i < 5; i++) {
+				client_sock = accept(server_sock, NULL,NULL);
+				printf("conected to client %dn",client_sock);
+				pthread_create(&tid[i],NULL,run_server,(void *)client_sock);
+			}
 		}
-			
-		printf("New client connected from port no %d and IP %s\n",ntohs(client.sin_port), inet_ntoa(client.sin_addr));
+		close(server_sock);
+    	} else {
+		printf("socket creation failed");
 	}
 
-		
-		
 	
 	return 0;
 }
